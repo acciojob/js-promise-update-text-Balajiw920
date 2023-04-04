5bef1fcc3652bef27611de7d2b4086d8ce75d4a0
Balajiw920 <wbalaji2021@gmail.com>
2023-04-04 23:35:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function delayedHello() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+async function updateOutput() {
+  const outputElement = document.getElementById("output");
+  const message = await delayedHello();
+  outputElement.innerText = message;
+}
+
+document.addEventListener("DOMContentLoaded", updateOutput);
